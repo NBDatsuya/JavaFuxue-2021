6704cd51ef1ec19a81762dd8cc8befea469a98df
NBDatsuya <2646809050@qq.com>
2022-04-22 17:43:49 +0800

    Huang Xuda OK

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,33 @@
-public abstract class Person {
+/**
+ * 
+ * @author Huang Xuda Samuel
+ *
+ */
+public class Person {
+	private int id;
+    private String name;
+    
+    public Person(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
     
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,3 +1,13 @@
-public class Teacher {
+public class Teacher extends Person{
     
+	private String phoneNumber;
+
+	public Teacher(int id,String name, String phoneNumber) {
+		super(id,name);
+		this.phoneNumber = phoneNumber;
+	}
+    public String getPhoneNumber() {
+    	return phoneNumber;
+    }
+	
 }

ac2fd269854cea92f465e5ec7b5181e20c7371a3
zhangke200377 <zhangke200377@outlook.com>
2022-04-30 23:52:30 +0800

    fix: 修复逻辑问题

--- complete/src/FileDatabaseConverter.java
+++ complete/src/FileDatabaseConverter.java
@@ -1,11 +1,11 @@
 public abstract class FileDatabaseConverter {
     public static final String DATA_DIVIDER = "_";
 
-    public void FileToDatabase(String fileData, DatabaseCountable databaseToWrite){
+    public void FileToDatabase(String fileToRead, DatabaseCountable databaseToWrite){
         throw new ActionNotAllowedException("This converter doesn't support this method");
     }
 
-    public String DatabaseToFile(DatabaseCountable database){
+    public String DatabaseToFile(DatabaseCountable database, String fileToWrite){
         throw new ActionNotAllowedException("This converter doesn't support this method");
     }
 
--- complete/src/LessonFileDatabaseConverter.java
+++ complete/src/LessonFileDatabaseConverter.java
@@ -1,2 +1,12 @@
 public class LessonFileDatabaseConverter extends FileDatabaseConverter{
+
+    @Override
+    public void FileToDatabase(String fileToRead, DatabaseCountable databaseToWrite) {
+
+    }
+
+    @Override
+    public String DatabaseToFile(DatabaseCountable database, String fileToWrite) {
+
+    }
 }
